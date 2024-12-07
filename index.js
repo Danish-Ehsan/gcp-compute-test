04d4d186fbcd396c6f8214d246e021e6fb32d500
Danish <danish.ehsan321@gmail.com>
2024-12-06 19:24:14 -0500

    Add an error path that throws an uncaught error

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send("Hello GCP Computer");
 });
 
+app.get("/error", (req, res) => {
+  (async () => {
+    console.log("throwing error");
+    throw new Error("This is an custom error");
+  })();
+});
+
 app.listen(80, () => {
   console.log("App listening on Port 80");
 });
